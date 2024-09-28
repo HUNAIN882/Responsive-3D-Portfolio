67586f2fb9fb026246843bcafd5a3bae8c4055ad
HUNAIN882 <hunaintoufeeq@gmail.com>
2024-09-27 17:04:18 -0700

    Add new parargraph

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const navs = document.querySelectorAll('.nav-list li');
 const cube = document.querySelector('.box');
+const sections = document.querySelectorAll('.section');
 
 const resumeLists = document.querySelectorAll('.resume-list');
 const resumeBoxs = document.querySelectorAll('.resume-box');
@@ -15,6 +16,20 @@ navs.forEach((nav, idx) => {
         nav.classList.add('active');
 
         cube.style.transform = `rotateY(${idx * -90}deg)`;
+
+        document.querySelector('.section.active').classList.remove('active');
+        sections[idx].classList.add('active');
+
+        const array  = Array.from(sections);
+        const arrSecs = array.slice(1, -1); // only requires indexes 1, 2, 3 or does not require start and end indexes
+        arrSecs.forEach(arrSec => {
+            if (arrSec.classList.contains('active')) {
+                sections[4].classList.add('action-contact');
+            }
+        });
+        if (sections[0].classList.contains('active')) {
+            sections[4].classList.remove('action-contact');
+        }
     });
 });
 
